c4ee9957bc3856fcbd3f6c266bbe5c57977fda38
Javier Daza <javierdazacomas@gmail.com>
2018-04-05 17:11:50 -0500

    Agregue ruta recomendada de cookiecutter-django

--- proyecto/proyecto/wsgi.py
+++ proyecto/proyecto/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+# This allows easy placement of apps within the interior
+# proyecto directory.
+app_path = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), os.pardir))
+sys.path.append(os.path.join(app_path, 'proyecto'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto.settings")
 
